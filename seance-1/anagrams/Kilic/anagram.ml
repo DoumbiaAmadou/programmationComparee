541e8ab784ac7e0db48cc0d944811a1781ac27e7
Kilic Ali-Firat <kilic.alifirat@gmail.com>
2015-01-10 16:30:34 +0100

    Arguments à mettre en ligne de commande

--- seance-1/anagrams/Kilic/anagram.ml
+++ seance-1/anagrams/Kilic/anagram.ml
@@ -1,5 +1,5 @@
-let create w = Hashtbl.create (String.length w);;
-  
+(* [hash_from_words table w o ] remplit la de hachage [table] avec le nombre 
+   d'occurrences de chaque lettre du mot [w]. *)
 let hash_from_words table w (o : int -> int -> int) =
   let add_characters table c =
     try
@@ -7,38 +7,48 @@ let hash_from_words table w (o : int -> int -> int) =
       Hashtbl.replace table c (o v 1)
     with Not_found -> Hashtbl.add table c 1 in
   String.iter (fun c -> add_characters table c) w;
-  table;;               
+  table;;
 
+(* [read_line i] retourne une ligne du fichier via le channel [i] et EOF si
+   on se trouve en fin de fichier. *)
 let read_line (i : in_channel) : string option =
   try Some (input_line i) with End_of_file -> None 
 
-let rec words_to_list i = match read_line i with
-  | None -> []
-  | Some s -> s :: words_to_list i
-                
+(* [make_words_list i] retourne la liste de mots contenus 
+   dans le fichier [src].*)
 let make_words_list src =
+  let rec words_to_list i = match read_line i with
+    | None -> []
+    | Some s -> s :: words_to_list i in
   let i = open_in src in
   words_to_list i 
 
-let is_anagram w1 w2 = 
+(* [is_anagram w1 w2] vérifie si les mots [w1] et [w2] sont des anagrammes.*)
+let is_anagram w1 w2 =
   if String.length w1 != String.length w2 then false
-  else 
-    let table = create w1 in
+  else
+    let table = Hashtbl.create (String.length w1) in 
     let table = hash_from_words table w1 (+) in
     let table = hash_from_words table w2 (-) in
     Hashtbl.fold (fun _ v acc -> acc && v = 0) table true
       
-let all_anagrams l =
+(* [anagrams l words] affiche tous les anagrammes des mots de [l] contenus
+   dans la liste [words]. *)
+let anagrams l words =
   List.iter (fun w ->
       Printf.printf "%s : " w;
       List.iter (fun w' ->
           if not(w = w') && is_anagram w w' then
             Printf.printf "%s " w'
-          else ()) l;
+          else ()) words;
       Printf.printf "\n"
-    ) l 
+    ) l
 
 let () =
-  let words = make_words_list "words" in
-  all_anagrams words
+  if Array.length Sys.argv > 1 then
+    let l = List.tl (Array.to_list Sys.argv) in
+    let words_list = make_words_list "words" in
+    anagrams l words_list
+  else
+    Printf.printf "Usage : ./anagram w1 w2 ... wn"
   
