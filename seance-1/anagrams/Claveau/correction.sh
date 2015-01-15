4d2ff6444c37e9972b83f9dc6612048bf713c572
claveau <claveau@informatique.univ-paris-diderot.fr>
2015-01-15 11:52:06 +0100

    ajout chmod sur compile.sh

--- seance-1/anagrams/Claveau/correction.sh
+++ seance-1/anagrams/Claveau/correction.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-ret = ""
+echo "toto 1"
+
 exec anagram toto chien > ret
-if $ret=="^toto:$^chien[(chien niche)(niche chien)]" then echo "OK" else echo "KO"
+
+ret = 'exec anagram toto chien'
+
+echo $ret
+
+if [[$ret=="^toto:$^chien[(chien niche)(niche chien)]$"]] then echo "OK" else echo "KO"
+
+echo "toto"
