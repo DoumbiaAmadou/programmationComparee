12e41413251765c3675fc09605b96046efc517a0
Regis-Gianas <yrg@pps.univ-paris-diderot.fr>
2015-01-15 08:40:25 +0100

    * Script de bench pour anagram.

---
+++ seance-1/anagrams/bench.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+function msg () {
+  normal='\033[0m'           
+  echo -e "${1}$2${normal}"             
+}
+
+function warn () {
+  red='\033[0;31m'
+  msg $red $1
+}
+
+function inform () {
+  green='\033[0;32m'
+  msg $green $1
+}
+
+for i in `find . -maxdepth 1 -mindepth 1 -type d`; do
+  cd $i
+  echo -n "Checking $i "
+  (chmod u+rx ./compile.sh && ./compile.sh) &> compile.log
+  if [ ! -e anagram ]; then warn "KO"; else inform "OK"; fi;
+  cd .. 
+done
+    
