90fc9ce030e90f078b860c7331a72274d02b3b2a
Keryan DIDIER <keryan.didier@gmail.com>
2015-01-31 12:32:17 +0100

    Correction + compile.sh

--- seance-2/ed/Didier/04-ed.c
+++ seance-2/ed/Didier/04-ed.c
@@ -9,7 +9,7 @@
  * Description de la lecture/ecriture bas niveau
 **/
 
-int fdin, fdout, ln=0;
+int fdin, fdout, ln=-1;
 
 int copy(const int dest){
   if(dest<ln){
@@ -17,11 +17,9 @@ int copy(const int dest){
     return 0;
   }
   char c[1];
-  while(ln<=dest){
-    if(!read(fdin,c,1)){
-      printf("mUh\n");
+  while(ln<dest){
+    if(!read(fdin,c,1))
       return 0;
-    }
     write(fdout,c,1);
     if(c[0]=='\n') ln++;
   }
@@ -35,11 +33,9 @@ int move(const int dest){
   }
   if(dest==ln) return 1;
   char c[1];
-  while(ln<=dest){
-    if(!read(fdin,c,1)){
-      printf("meh\n");
+  while(ln<dest){
+    if(!read(fdin,c,1))
       return 0;
-    }
     if(c[0]=='\n') ln++;
   }
   return 1;
@@ -60,23 +56,23 @@ void apply(command c){
   case 'I':
     if(copy(c.arg1))
       write(fdout,c.text,c.tlen);
-    else exit(1);
+    else exit(EXIT_FAILURE);
     break;
   
   case 'D':
-    if(copy(c.arg1) && move(c.arg2));
-    else exit(1);
+    if(copy(c.arg1-1) && move(c.arg2));
+    else exit(EXIT_FAILURE);
     break;
   
   case 'R':
-    if(copy(c.arg1) && move(c.arg2))
+    if(copy(c.arg1-1) && move(c.arg2))
       write(fdout,c.text,c.tlen);
     else
-      exit(1);
+      exit(EXIT_FAILURE);
     break;
     
   default:
-    exit(1);
+    exit(EXIT_FAILURE);
     break;
     
   }
@@ -95,22 +91,22 @@ command read_cmd(){
   c.code = buf[0];
   
   int s=0,i;
-  for(i = 3; buf[i]>=48 && buf[i]<=57; i++)
+  for(i = 2; buf[i]>=48 && buf[i]<=57; i++)
     s=s*10+(buf[i]-48);
   c.arg1 = s;
   
   if(c.code=='D' || c.code=='R'){
     s=0;
-    for(i += 2; buf[i]>=48 && buf[i]<=57; i++)
+    for(++i; buf[i]>=48 && buf[i]<=57; i++)
       s=s*10+(buf[i]-48);
     c.arg2 = s;
   }
   else c.arg2 = -1;
   
   if(c.code=='I' || c.code=='R'){
-    int start = i+2;
+    int start = i+1;
     char text[80];
-    for(i+=2; i<start+80; i++){
+    for(++i; i<start+80; i++){
       text[i-start] = buf[i];
       if(buf[i]=='\n') break;
     }
@@ -138,7 +134,7 @@ void ed(){
 int main(int argc, char** argv){
   if(argc < 3) exit(1);
   fdin = open(argv[1],O_RDONLY);
-  fdout = open(argv[2],O_WRONLY|O_CREAT,S_IRWXU);
+  fdout = open(argv[2],O_WRONLY|O_CREAT|O_TRUNC,S_IRWXU);
   ed();
   close(fdin);
   close(fdout);
