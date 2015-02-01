a03b23b707b87d277bcd99422ab460ef4f642a32
Kilic Ali-Firat <kilic.alifirat@gmail.com>
2015-02-01 13:17:08 +0100

    Kilic version ed 2 bis

--- seance-2/ed/Kilic/kilic-ed.c
+++ seance-2/ed/Kilic/kilic-ed.c
@@ -5,23 +5,74 @@
 
 #define BUFSIZE 80
 
-void read_instruction(char buf[BUFSIZE]);
+struct cmd {
+  char c;
+  int arg1;
+  int arg2;
+  char text[BUFSIZE];
+}cmd;
 
-void interpret_instruction(char buf[BUFSIZE]);
+void read_instruction(struct cmd *c);
+
+void interpret_instruction(struct cmd *c);
+
+void print_command(struct cmd *c);
 
 int main(int argc, char **argv) {
-  char buf[BUFSIZE];
-  do {
-    read_instruction(buf);
-    interpret_instruction(buf);
-  }while(buf[0] != 'E');
+  struct cmd c;
+  read_instruction(&c);
+  print_command(&c);
+  interpret_instruction(&c);
   return 0;
 }
 
-void read_instruction(char buf[BUFSIZE]) {
+void read_instruction(struct cmd *c) {
+  char line[BUFSIZE], *tmp;
+  ssize_t r;
+  int arg,i;
+  r = read(STDIN_FILENO,line,BUFSIZE);
+  if(r > BUFSIZE) {
+    printf("Error : the MAX length of your command must be 80. Goodbye !");
+    exit(EXIT_FAILURE);
+  }
+  line[r] = '\0';
+  tmp = line;
+  cmd.c = line[0];
+  tmp++; tmp++;
+  arg = 0;
+  if(line[0] == 'E') return;
+  while(*tmp != ',') {
+    arg = arg * 10 + (*tmp - '0');
+    tmp++;
+  }
+  cmd.arg1 = arg;
+  tmp++;
+  if(line[0] == 'R' || line[0] == 'D') {
+    arg = 0;
+    while(*tmp != ',' && *tmp != '\n') {
+      arg = arg * 10 + (*tmp - '0');
+      tmp++;
+    }
+    if(*tmp == ',') tmp++;
+    cmd.arg2 = arg;
+  }
+  if(line[0] == 'I' || line[0] == 'R') {
+    i = 0;  
+    while(*tmp != '\n') {
+      cmd.text[i++] = *tmp;
+      tmp++;
+    }
+  }
 }
 
-void interpret_instruction(char buf[BUFSIZE]) {
+void interpret_instruction(struct cmd *c) {
+}
+
+void print_command(struct cmd *c) {
+  printf("Command = %c\n", cmd.c);
+  printf("Argument 1 = %d\n", cmd.arg1);
+  printf("Argument 2 = %d\n", cmd.arg2);
+  printf("Text = %s\n", cmd.text);
 }
 
 
