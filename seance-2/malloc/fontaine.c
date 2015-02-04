c92edbff9bbacc7de304ede0a54ddbe56f65d9a8
Baptiste Fontaine <batifon@yahoo.fr>
2015-02-04 23:14:38 +0100

    Doc for Fontaine's malloc/free broken implementation
    Djibee vous accueille à bras ouverts. Vous finissez votre vie en discutant
    d’interfaces graphiques en Objective-C tout en vous nourrissant exclusivement
    de M&M's.

--- seance-2/malloc/fontaine.c
+++ seance-2/malloc/fontaine.c
@@ -5,8 +5,29 @@
  * A dead-simple malloc/free implementation, using only sbrk(2). It works as a
  * stack; you can only free the last malloc'ed pointer.
  *
- * This could be circumvented by using a global register, but `didier.c`
+ * This could be circumvented by using a global register, but didier.c
  * already did it and it might be interesting to try other ways.
+ *
+ * It works by prepending `sizeof(size_t)` bytes to any malloc-ed pointer. The
+ * size of the memory pointed out by the pointer is stored in these bytes.
+ * `bf_free` can then reduce the used memory by decrementing the data space
+ * using this size.
+ *
+ * Limitations:
+ * - It works as a stack: you can only free the last malloc-ed space, or it'll
+ *   break the program
+ * - There's nothing preventing the user from writing outside of a malloc-ed
+ *   memory space if there's something after it, e.g.:
+ *
+ *      int *p1 = bf_malloc(sizeof(int)),
+ *          *p2 = bf_malloc(sizeof(int));
+ *
+ *      *(p1 + 3) = 42;
+ *      printf("%d\n", *p2); // 42
+ *
+ * - It works with a contiguous memory space only
+ *
+ * A better implementation would use mmap(2) with a thread-proof register.
  **/
 
 #define BF_SIZESIZE (sizeof (size_t))
@@ -22,7 +43,7 @@ void *bf_malloc(size_t size) {
 
 // FIXME doesn't work (sbrk fails)
 int bf_free(void *p) {
-        void *ret = sbrk(-(*(((size_t*)p) - 1) + BF_SIZESIZE));
+        void *ret = sbrk(-(BF_SIZEOF(p) + BF_SIZESIZE));
         return (ret != (void*)-1);
 }
 
