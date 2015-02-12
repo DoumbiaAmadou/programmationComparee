d30335d1c8a55c24d9c6c5d0a5b93c4a20f5cbf7
Baptiste Fontaine <batifon@yahoo.fr>
2015-02-12 11:39:41 +0100

    Fontaine's LISPy pow
    Vous entrez dans un hall sombre où un sosie féminin de Richard Stallman vous
    accueille derrière un comptoir avec un grand sourire. Vous remarquez un
    écriteau étrange à côté d’elle, comme si les longues années avaient effacé une
    partie de son message. Vous décidez de le lire, rendez-vous en <10.10>. Vous n’en
    avez décidément rien à cirer, rendez-vous en <14.04>.

--- seance-3/lisp/pow/fontaine/pow.clj
+++ seance-3/lisp/pow/fontaine/pow.clj
@@ -1,5 +1,10 @@
-(defn pow
+(defmacro pow-macro
   "creates a function which takes an `x` and return `x^n`"
   [n]
-  (fn [x]
-    (Math/pow x n)))
+  `(fn [x#]
+    (Math/pow x# '~n)))
+
+(defn pow-fun
+  "returns a function which takes an `x` and return `x^n`"
+  [n]
+  #(Math/pow % n))
