766e722ac92818f740f092a686a674143ce703ab
NGUYEN thi ngoc tam <nguyen3@informatique.univ-paris-diderot.fr>
2015-02-12 12:25:27 +0100

    anagrams

---
+++ seance-3/lisp/anagrammes-le-retour/NguyenTam/anagrams.lisp
@@ -0,0 +1,33 @@
+;; reference version
+
+(defun anagrams (&optional (path "../words"))
+  (let ((words (drakma:http-request path :want-stream t))
+        (wordsets (make-hash-table :test 'equalp)))
+
+    ;; populate the wordsets and close stream
+    (do ((word (read-line words nil nil) (read-line words nil nil)))
+        ((null word) (close words))
+      (let ((letters (sort (copy-seq word) 'char<)))
+        (multiple-value-bind (pair presentp)
+            (gethash letters wordsets)
+          (if presentp
+           (setf (car pair) (1+ (car pair))
+                 (cdr pair) (cons word (cdr pair)))
+           (setf (gethash letters wordsets)
+                 (cons 1 (list word)))))))
+
+    ;; find and return the biggest wordsets
+    (loop with maxcount = 0 with maxwordsets = '()
+          for pair being each hash-value of wordsets
+          if (> (car pair) maxcount)
+          do (setf maxcount (car pair)
+                   maxwordsets (list (cdr pair)))
+          else if (eql (car pair) maxcount)
+          do (push (cdr pair) maxwordsets)
+          finally (return (values maxwordsets maxcount)))))
+
+;; evalutating
+;; (multiple-value-bind (wordsets count) (anagrams)
+;;  (pprint wordsets)
+;;  (print count))
+
