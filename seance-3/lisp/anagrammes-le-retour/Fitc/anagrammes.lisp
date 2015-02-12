a30bad9ecc4a1716bb1c4a48f5621ba4c7304f27
Vladislav Fitc <Vladislav.Fitz@gmail.com>
2015-02-12 12:13:51 +0100

    Anagram version finale

--- seance-3/lisp/anagrammes-le-retour/Fitc/anagrammes.lisp
+++ seance-3/lisp/anagrammes-le-retour/Fitc/anagrammes.lisp
@@ -1,18 +1,15 @@
 #! /usr/local/bin/clisp
-
-;Read programm arg
-; (format t "~&~s~&" *args*)
-
-; (let ((the-string (copy-seq "this is the string")))
-;   (sort the-string #'char-lessp))
-
-
-(setq wds (make-hash-table))
-(defun string-sorter(s) (sort (copy-seq s) #'char-lessp))
+;(setq wds (make-hash-table))
 ; (defun put-string-to-dict(s) (setf (gethash (STRINGSORTER s)))) 
 
+;Sort string in lexicographical order
+(defun string-sorter(s) (sort (copy-seq s) #'char-lessp))
+
+;Return true if s1 and s2 are anagrams
 (defun check-anagram(s1 s2) (string-equal (string-sorter s1) (string-sorter s2))) 
+
+;Inefficace mais marche bien quand même
 (with-open-file (stream "../words")
     (loop for line = (read-line stream nil 'foo)
           until (eq line 'foo)
-          do (if (check-anagram line "aborder") (print line) (continue))))
+          do (if (check-anagram line (car *args*)) (print line) (continue))))
