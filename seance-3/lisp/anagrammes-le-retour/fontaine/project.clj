a3279d5b4f07e5aa9781d557023a29513fa73c05
Baptiste Fontaine <batifon@yahoo.fr>
2015-02-12 10:33:21 +0100

    Fontaine's LISPy anagrams
    L’écriteau, une fois nettoyé, ne donne rien. Vous décidez de le scanner et
    d’explorer les bits de poids faible pour voir s’il n’y a pas un message codé,
    rendez-vous en <m64>. Vous vous dites que vous perdez votre temps, et vous avez
    bien raison, rendez-vous en <10.10>.

---
+++ seance-3/lisp/anagrammes-le-retour/fontaine/project.clj
@@ -0,0 +1,7 @@
+(defproject fontaine "0.1.0-SNAPSHOT"
+  :description "Anagrams: WE'LL BE BACK"
+  :url "http://buvezducoca.com"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main fontaine.core
+  :dependencies [[org.clojure/clojure "1.6.0"]])
