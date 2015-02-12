24cf2d0356536f87b3e356c8fed26b252bda297c
Yann Régis-Gianas <yrg@pps.univ-paris-diderot.fr>
2015-02-12 08:35:16 +0100

    Seance 3.

--- seance-2/ed/Charasson/01-ed.c
+++ seance-2/ed/Charasson/01-ed.c
@@ -14,20 +14,20 @@ char out_path[4096];
 
 
 struct s_usr_instr{
-	char code;
-	char ch;
-	int m;
-	int v;
+        char code;
+        char ch;
+        int m;
+        int v;
 };
 typedef struct s_usr_instr usr_instr;
 
 /* read_usr
  *---------
- *	lit les instruction de l'utilisateur dans l'entrée standard
+ *      lit les instruction de l'utilisateur dans l'entrée standard
  *  */
 int read_usr(usr_instr *i){
-	//TODO
-	return EXIT_SUCCESS; 	
+        //TODO
+        return EXIT_SUCCESS;
 }
 
 /* interpret
@@ -35,8 +35,8 @@ int read_usr(usr_instr *i){
  * interprète l'instruction de l'utilisateur
  *  */
 int interpret(usr_instr *i){
-	//TODO
-	return EXIT_SUCCESS;
+        //TODO
+        return EXIT_SUCCESS;
 }
 
 /* editor
@@ -44,16 +44,16 @@ int interpret(usr_instr *i){
  * boucle d'interraction avec l'utilisateur
  * */
 int editor(FILE *in, FILE *out){
-	int n;
-	int ln;
-	usr_instr instr;
+        int n;
+        int ln;
+        usr_instr instr;
 
-	do{
-		read_usr(&instr);
-		interpret(&instr);
-	}while(instr != 'E');
+        do {
+          read_usr(&instr);
+          interpret(&instr);
+        } while (instr != 'E');
 
-	return EXIT_SUCCESS;
+        return EXIT_SUCCESS;
 }
 
 /* parse_arg
@@ -61,12 +61,12 @@ int editor(FILE *in, FILE *out){
  * ouvre les fichiers, parse les options
  * */
 int parse_arg(int argc, char** argv){
-	return EXIT_SUCCESS;
+        return EXIT_SUCCESS;
 }
 
 
 int main(int argc, char* argv[]){
-	parse_arg(argc,argv);
-	editor(in,out);
-	return EXIT_SUCCESS;
+        parse_arg(argc,argv);
+        editor(in,out);
+        return EXIT_SUCCESS;
 }
