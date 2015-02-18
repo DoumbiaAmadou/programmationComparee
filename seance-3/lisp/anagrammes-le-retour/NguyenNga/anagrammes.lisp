a930d4078e76bc98e2ce04bdf5a082c1d75ebb34
NGUYEN thi quynh nga <nguyen4@informatique.univ-paris-diderot.fr>
2015-02-19 00:13:41 +0100

    anagramme presque fini

--- seance-3/lisp/anagrammes-le-retour/NguyenNga/anagrammes.lisp
+++ seance-3/lisp/anagrammes-le-retour/NguyenNga/anagrammes.lisp
@@ -7,10 +7,34 @@
    (let ((in (open filename)))
      (when in
        (loop for line = (read-line in nil)
-	     while line do ( nconc *listWords* (list line) )
-	)
-      )
-     (close in)
-     )
+	     while line do ( nconc *listWords* (list line))))
+     (close in))
    *listWords*)
+;test getWords
 (getWords "wordsTest")
+
+;sort string
+(defun sortWord (word)
+   "return the string sorted"
+   (sort (copy-seq word) #'char>))
+;test sortWord
+(sortWord "ohno")
+
+
+;compaire two words
+(defun compaireWord (w1 w2)
+  "return true if two words are equivalent"
+  (string-equal (sortWord w1) (sortWord w2)))
+;test compaireWords
+(compaireWords "ohno" "nooh")
+
+;make hash-table
+(defun makeHashTable (words)
+  "return a hash-table"
+  (setq hashWords (make-hash-table))
+  (loop for word on words do 
+    ((= (gethash (sortWord word) hashWords) ))
+    (setf )))
+
+;main function
+(defun anagrammes ())
