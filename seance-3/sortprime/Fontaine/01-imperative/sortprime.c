41541985c6afeb3c462685a285ea513fecb24f06
Baptiste Fontaine <batifon@yahoo.fr>
2015-02-19 12:29:56 +0100

    init sortprime bfontaine (doesn't work)

--- seance-3/sortprime/Fontaine/01-imperative/sortprime.c
+++ seance-3/sortprime/Fontaine/01-imperative/sortprime.c
@@ -1,10 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
-#define MEM_SIZE (640*1024)/(sizeof int)
+#define MEM_SIZE (640*1024)
+#define BUF_SIZE 1024
 
-int mem[MEM_SIZE];
+#define MAX_NUM ((MEM_SIZE)-(BUF_SIZE)/2)
 
-int main(void) {
+char numbers[MAX_NUM],
+     counts[MAX_NUM];
+
+void eratosthene(int n) {
+        if (n <= 1) return;
+
+        for (int i=n*2; i<MAX_NUM; i += n) {
+                numbers[n] = 0;
+        }
+}
+
+void init(void) {
+        for (int i=0; i<MAX_NUM; ++i) {
+                numbers[i] = 1;
+                counts[i] = 0;
+        }
+}
+
+void add_number(int n) {
+        if (numbers[n]) { ++counts[n]; }
+}
+
+void print_list(void) {
+        for (int i=0; i<MAX_NUM; ++i) {
+                for (int j=0; j<counts[i]; ++j) {
+                        printf("%d\n", numbers[i]);
+                }
+        }
+}
+
+int main(int argc, char **argv) {
+        if (argc != 2) {
+                fprintf(stderr, "Usage:\n\tgenrandom <k> | %s <k>\n", argv[0]);
+                exit(1);
+        }
+
+        init();
+
+        int line = 0,
+            n,
+            ret;
+
+        while ((ret = scanf("%d\n", &n)) == 1) {
+                eratosthene(line);
+                add_number(n);
+                line++;
+        }
+
+        print_list();
 
         return 0;
 }
