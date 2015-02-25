3f1b7e292924bac16bb8fc045eb2c8670bb34070
Kilic Ali-Firat <kilic.alifirat@gmail.com>
2015-02-25 21:44:54 +0100

    Message d'erreurs en cas d'arguments trop nombreux

--- seance-3/sortprime/Kilic/dynamic-size/sortprime-dyn.c
+++ seance-3/sortprime/Kilic/dynamic-size/sortprime-dyn.c
@@ -14,26 +14,32 @@ int main(int argc, char **argv) {
   long long int value;  
   char *t;
   long long int sorted_primes[SIZE];
-  memset(sorted_primes,0,SIZE);
-  /* Reading the given value until the end */
-  while(scanf("%lld",&value) == 1) {
-    assert(value < SIZE);
-    bool = is_prime(&t,value);
-    printf("%lld is primer ? %d\n", value,bool);
-    /* If [value] is prime, we add into the array of prime numbers array */
-    if(bool) {
-      sorted_primes[index++] = value;      
+  if(argc > 1) {
+    fprintf(stderr, "Usage : ./genrandom N | %s\n",argv[0]);
+    exit(EXIT_FAILURE);
+  } else {
+    memset(sorted_primes,0,SIZE);
+    /* Reading the given value until the end */
+    while(scanf("%lld",&value) == 1) {
+      assert(value < SIZE);
+      bool = is_prime(&t,value);
+      printf("%lld is primer ? %d\n", value,bool);
+      /* If [value] is prime, we add into the array of prime numbers array */
+      if(bool) {
+	sorted_primes[index++] = value;      
+      }
+      free(t);
     }
-    free(t);
+    /* We use the quick sort algorithm to sort our array of sorted prime 
+       numbers. No need to sort the all values in the array, only these we met 
+       before.*/
+    qsort(&sorted_primes, index, sizeof(long long int), compare);
+    printf("The list of all prime numbers that we met : ");
+    for(i = 0; i < index; i++) {
+      printf("%lld ", sorted_primes[i]);
+    }
+    return 0;
   }
-  /* We use the quick sort algorithm to sort our array of sorted prime numbers. 
-     No need to sort the all values in the array, only these we met before.*/
-  qsort(&sorted_primes, index, sizeof(long long int), compare);
-  printf("The list of all prime numbers that we met : ");
-  for(i = 0; i < index; i++) {
-    printf("%lld ", sorted_primes[i]);
-  }
-  return 0;
 }
 
 /* [is_prime tab value] check if [value] is a prime number */
--- seance-3/sortprime/Kilic/static-size/sortprime-static.c
+++ seance-3/sortprime/Kilic/static-size/sortprime-static.c
@@ -9,31 +9,36 @@
 int compare(const void *x, const void *y);
 void set_prime_numbers(char tab[]);
 
-
 int main(int argc, char **argv) {
   int i = 0, index = 0;
   long long int value;  
   char t[SIZE];
   long long int sorted_primes[SIZE];
-  memset(sorted_primes,0,SIZE);
-  set_prime_numbers(t);
-  /* Reading the given value until the end */
-  while(scanf("%lld",&value) == 1) {
+  if(argc > 1) {
+    fprintf(stderr, "Usage : ./genrandom N | %s\n",argv[0]);
+    exit(EXIT_FAILURE);
+  } else {
+    memset(sorted_primes,0,SIZE);
+    set_prime_numbers(t);
+    /* Reading the given value until the end */
+    while(scanf("%lld",&value) == 1) {
     assert(value < SIZE);
     printf("%lld is primer ? %d\n", value, t[value]);
     /* If [value] is prime, we add into the array of prime numbers array */
     if(t[value]) {
       sorted_primes[index++] = value;      
     }
+    }
+    /* We use the quick sort algorithm to sort our array of sorted prime 
+       numbers. No need to sort the all values in the array, only these we met 
+       before.*/
+    qsort(&sorted_primes, index, sizeof(long long int), compare);
+    printf("The list of all prime numbers that we met : ");
+    for(i = 0; i < index; i++) {
+      printf("%lld ", sorted_primes[i]);
+    }
+    return 0;
   }
-  /* We use the quick sort algorithm to sort our array of sorted prime numbers. 
-   No need to sort the all values in the array, only these we met before.*/
-  qsort(&sorted_primes, index, sizeof(long long int), compare);
-  printf("The list of all prime numbers that we met : ");
-  for(i = 0; i < index; i++) {
-    printf("%lld ", sorted_primes[i]);
-  }
-  return 0;
 }
 
 /* [set_prime_number tab] keeps only the prime number of the array [tab]. */
