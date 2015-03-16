891e250eb074185b434f815a0ebad014ddbb716e
Dias Alain <alain.dias02@gmail.com>
2015-03-16 23:35:52 +0100

    les fourmis ont le behavior forwarder [Phase de test]

--- projet/noCaml/src/main/scala/ants/ia/Queen.scala
+++ projet/noCaml/src/main/scala/ants/ia/Queen.scala
@@ -9,19 +9,22 @@ import ants.Controlled
 import comm.Parse
 import comm.parse.Observations
 import ants.behaviors.RandomExplorer
+import ants.behaviors.Forwarder
   
 
-class Queen (val parse : Parse[_]) {
+class Queen (val parse : Parse[_])]) {
+this(parse)super()
+   ()
   
-   def turn(world : WorldMap) : Observations = { 
+   def turn(world : WorldMap): Observations = { 
     val ants = this antsInit world
     
     val commands = ants.filter {
-      _ match {
-        case ant : Ant with Controlled => true
-        case _ => false
-      }
-    }.map {
+        _ match {
+          case ant : Ant with Controlled => true
+          case _ => false
+        }
+      }.map
       _ match {
         case ant : Ant with Controlled => (ant.numAnt, ant.playTurn(world))
         case _ => throw new Exception("Queen.turn exception")
@@ -31,11 +34,11 @@ class Queen (val parse : Parse[_]) {
     parse.play(commands)
   }
 
-  private def antsInit(world : WorldMap) : List[Ant] = {
-    def aux(states : List[AntState]) : List[Ant] = {
+  private def antsInit(world : WorldMap): List[Ant] = {
+    def aux(states : List[AntState]): List[Ant] = {
       states match {
         case Nil => Nil
-        case a::t => AntFactory.make(a, RandomExplorer):: aux(t) //TODO: Autres comportements
+        case a::t => AntFactory.make(a, Forwarde):: AntFactory.make(a, Forwarder):: aux(t) //TODO: Autres comportements
       }
     }
     aux(world.ants)
