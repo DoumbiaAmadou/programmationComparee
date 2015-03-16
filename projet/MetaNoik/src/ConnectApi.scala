134c948479556d5e6a72281a050ea1080c0042fb
NGUYEN Thi Quynh Nga <quynhnga.k59spt@gmail.com>
2015-03-16 12:40:47 +0100

    commanter les codes pour sbt run

--- projet/MetaNoik/src/ConnectApi.scala
+++ projet/MetaNoik/src/ConnectApi.scala
@@ -7,16 +7,16 @@ object ConnectApi {
 	// Send request by method POST
 	def requestPOST(requestName: String, params: Seq[(String, String)]) : String = {
 		val request: HttpRequest = Http(baseUrl+version+"/"+ requestName +"?")
-      	val response: HttpResponse[String] = request.postForm(params).asString
-      	return response.body
+      		val response: HttpResponse[String] = request.postForm(params).asString
+      		return response.body
 	}
 
 	// Send request by method GET
 	def requestGET(requestName: String, params: ((String,String))) : String = {
 		val request: HttpRequest = Http(baseUrl+version+"/"+ requestName +"?")
 		println(request)
-      	val response: HttpResponse[String] = request.params(params).asString
-      	return response.body
+      		val response: HttpResponse[String] = request.params(params).asString
+      		return response.body
 	}
 
 	// Connect a user
@@ -27,36 +27,35 @@ object ConnectApi {
 	}
 
 	// Create a new game. -- probleme: can't send request with params int
-	def newGame(users: String, teaser: String, pace: String , nb_turn: Int, nb_ant_per_player: Int, nb_player: Int,
-				 minimal_nb_player: Int, initial_energy: Int, initial_acid: Int) : String = {
-		val request: HttpRequest = Http(baseUrl+version+"/create?")
-		var postform = Seq("users"->users,"teaser"->teaser,"pace"->pace, "nb_turn"->nb_turn, 
-							"nb_ant_per_player"->nb_ant_per_player, "nb_player"->nb_player, 
-							"minimal_nb_player"->minimal_nb_player, "initial_energy"->initial_energy,
-							"initial_acid"->initial_acid)
-      	val response: HttpResponse[String] = request.params(postform).asString
-      	return response.body
-	}
+	//	def newGame(users: String, teaser: String, pace: String , nb_turn: Int, nb_ant_per_player: Int, nb_player: Int,
+		//			 minimal_nb_player: Int, initial_energy: Int, initial_acid: Int) : String = {
+		//	val request: HttpRequest = Http(baseUrl+version+"/create?")
+		//	var postform = Seq("users"->users,"teaser"->teaser,"pace"->pace, "nb_turn"->nb_turn, 
+		//						"nb_ant_per_player"->nb_ant_per_player, "nb_player"->nb_player, 
+		//						"minimal_nb_player"->minimal_nb_player, "initial_energy"->initial_energy,
+		//						"initial_acid"->initial_acid)
+      	//	val response: HttpResponse[String] = request.params(postform).asString
+      	//	return response.body
+	//	}
 
 	// Destroy a game.
-	def destroyGame(idGame: String) : String = {
-		var params =(("id", idGame))
-		println(params)
-		return requestGET("destroy",params)
-	}
+	//	def destroyGame(idGame: String) : String = {
+	//		var params =(("id", idGame))
+	//		println(params)
+	//		return requestGET("destroy",params)
+	//	}
 
 	// List all visible games.
-	def listGame(): String = {
-		return requestGET("game", null)
-	}
+	//	def listGame(): String = {
+	//		return requestGET("game", null)
+	//	}
 
 
 	
 	// Test connect
 	def main(args: Array[String]) {
-      	println("Hello, scala request!")
-
-		println(auth("nga","ngango"))
+      	    println("Hello, scala request!")
+	    println(auth("nga","ngango"))
 		// println(newGame("nga", "ngo", 10,30,1,1,1,100,100))
 		// println(destroyGame("1"))
 		// println(requestGET("auth",))
