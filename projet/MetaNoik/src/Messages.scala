5f31da14555cdb74cb45318420e57a244ac8e648
Nathan Rebours <nathan.rebours@tamere.fr>
2015-03-18 19:16:19 +0100

    Updated Message class

--- projet/MetaNoik/src/Messages.scala
+++ projet/MetaNoik/src/Messages.scala
@@ -1,7 +1,19 @@
 import akka.actor
 
-(* server Interactions *)
-case class Auth(login: String, pass: String)
+abstract class Command
+
+case class Left(id: Int)
+case class Right(id: Int)
+case class Forward(id: Int)
+case class Rest(id: Int)
+case class Attack(id: Int, power: Int)
+case class Hack(id: Int, code: String)
+
+abstract class Message
+
+/* server Interactions */
+/* IN */
+case class Auth(login: String, pass: String) extends Message
 case class Create(users: String,
 		  teaser: String,
 		  pace: Int,
@@ -10,15 +22,20 @@ case class Create(users: String,
 		  nb_player: Int,
 		  min: Int,
 		  energy: Int,
-		  acid: Int)
-case class Destroy(id: String)
-case class Join(id: String)
-case class Logout()
+		  acid: Int) extends Message
+case class Destroy(id: String) extends Message
+case class Join(id: String) extends Message
+case class Logout() extends Message
+case class Play(id: Int, cmds: List[Pair[Int,Command], status: Status) extends Message
 
-(* ant actions *)
-case class Left(id: Int, etat: Etat)
-case class Right(id: Int, etat: Etat)
-case class Forward(id: Int, etat: Etat)
-case class Rest(id: Int, etat: Etat)
-case class Attack(id: Int, lvl: Int, etat: Etat)
-case class Hack(id: Int, code: String, etat: Etat)
+/* OUT */
+case class Observation(turn: Int, 
+		       obs: List[Pair[AntState,Map]], 
+		       status: Status) extends Message
+
+
+
+case class Compute(obs: List[Pair[AntState,Map]], status: Status)
+
+case class Ants(obs: List[Pair[AntState,Map]], status: Status)
+case class Map(obs: List[Pair[AntState,Map]], status: Status)
