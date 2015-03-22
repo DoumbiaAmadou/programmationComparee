e38e0ef0c904ff9b85e294d94f78113b32b220c5
Kilic Ali-Firat <kilic.alifirat@gmail.com>
2015-03-22 23:32:36 +0100

    Ajout d'une fonction qui nettoie la div

--- projet/DHK/utils.ml
+++ projet/DHK/utils.ml
@@ -7,12 +7,19 @@ let get_element_by_id id =
   Printf.printf "id = %s\n" id;
   Js.Opt.get (doc##getElementById (Js.string id)) (fun () -> assert false)
 
-
 let input_value id =
   let input = get_element_by_id id in
   let input = Js.Unsafe.coerce input in 
-  (* match Js.Opt.to_option (input##textContent) with *)
-  (* | None -> assert false *)
-  (* | Some s -> Js.to_string s  *)
-  Js.to_string (input##value) 
-  
+  Js.to_string (input##value)
+
+let clear_div div_id =
+  let div = get_element_by_id div_id in
+  Dom.list_of_nodeList (div##childNodes)
+  |> List.iter (fun child -> Dom.removeChild div child)
+
+let create_input id value =
+  let input = Html.createInput doc ~_type:(Js.string "radio") in
+  input##id <- Js.string id;
+  input##value <- Js.string value;
+  input
+
