b9d64f8e9523b9798e776dce010dfa8ca3b0fa0f
Vladislav Fitc <Vladislav.Fitz@gmail.com>
2015-03-22 22:07:13 +0100

    surroundings fix

--- projet/ANTirvirusse/src/game_map.py
+++ projet/ANTirvirusse/src/game_map.py
@@ -55,7 +55,6 @@ class GameMap():
 		surroundings_coordinates = [(x-1, y-1),
 									(x-1, y),
 									(x-1, y+1),
-									(x-1, y+1),
 									(x 	, y-1),
 									(x 	, y+1),
 									(x+1, y-1),
