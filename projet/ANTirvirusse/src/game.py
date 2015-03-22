310c0095863e4b012fe0247eeddf4445af3c657f
Vladislav Fitc <Vladislav.Fitz@gmail.com>
2015-03-22 12:53:25 +0100

    Les fourmis sont placés sur game_map

--- projet/ANTirvirusse/src/game.py
+++ projet/ANTirvirusse/src/game.py
@@ -16,15 +16,9 @@ class Game():
 
         self.gid            = gid
         self.pace           = pace
-        self.ants           = []
-        self.game_map       = GameMap()
         self.initial_acid   = initial_acid
         self.initial_energy = initial_energy
-
-        for ant_id in range(0, nb_ants):
-            ant = Ant(ant_id, initial_energy, initial_acid, True)
-            self.ants.append(ant)
-
+        self.game_map       = GameMap(nb_ants, initial_energy, initial_acid)
         self.make_move()
 
     def get_initial_acid(self):
@@ -36,38 +30,13 @@ class Game():
     def get_map(self):
         return self.game_map
 
-    def get_ants(self):
-        return self.ants
-
-    def get_ant_with_id(self, ant_id):
-        search_result = filter(lambda ant: ant.ant_id == ant_id, self.ants)
-        if len(search_result) > 0:
-            return search_result[0]
-        else:
-            return None
-
     def destroy(self):
         nl.game_destroy(self.gid)
 
     def make_move(self):
-        commands = map(lambda ant: ant.get_attached_command(), self.ants)
+        commands = map(lambda ant: ant.get_attached_command(), self.game_map.get_ants())
         ants_stats = nl.game_play(self.gid, commands)
-        self.game_map.update(ants_stats)
-
-        for stat in ants_stats:
-            ant_state   = stat[0]
-            ant_id      = ant_state["id"]
-            current_ant = self.get_ant_with_id(ant_id)
-
-            if current_ant is not None:
-                current_ant.set_energy(int(ant_state["energy"]))
-                current_ant.set_acid(int(ant_state["acid"]))
-                current_ant.set_brain(ant_state["brain"]=="controlled")
-                current_ant.set_position(int(ant_state["x"]), int(ant_state["y"]))
-
-                print(current_ant)
-
-        print(self.game_map)
+        self.game_map.update(ants_stats, verbose=True)
 
     def show_status(self):
         print nl.game_status(self.gid)
--- projet/ANTirvirusse/src/game_map.py
+++ projet/ANTirvirusse/src/game_map.py
@@ -1,10 +1,16 @@
  # -*- coding: utf-8 -*-
 from case import Case
+from ant import Ant
 
 class GameMap():
 
-	def __init__(self):
+	def __init__(self, nb_ants, initial_energy, initial_acid):
 		self.map_dict = {}
+		self.ants     = []
+
+		for ant_id in range(0, nb_ants):
+		    ant = Ant(ant_id, initial_energy, initial_acid, True)
+		    self.ants.append(ant)
 
 	def __str__(self):
 		text_repr = ""
@@ -14,11 +20,36 @@ class GameMap():
 
 		return text_repr
 
-	def update(self, ants_stats):
+	def get_ants(self):
+	    return self.ants
+
+	def get_ant_with_id(self, ant_id):
+	    search_result = filter(lambda ant: ant.ant_id == ant_id, self.ants)
+	    if len(search_result) > 0:
+	        return search_result[0]
+	    else:
+	        return None
+
+	def update(self, ants_stats, verbose=False):
 		for stat in ants_stats:
 			cases = map(lambda case_dict: Case(case_dict["content"]["kind"], int(case_dict["x"]), int(case_dict["y"])), stat[1])
 			for case in cases:
 				self.map_dict[(case.x, case.y)] = case
+
+			ant_state   = stat[0]
+			ant_id      = ant_state["id"]
+			current_ant = self.get_ant_with_id(ant_id)
+
+			if current_ant is not None:
+				current_ant.set_energy(int(ant_state["energy"]))
+				current_ant.set_acid(int(ant_state["acid"]))
+				current_ant.set_brain(ant_state["brain"]=="controlled")
+				current_ant.set_position(int(ant_state["x"]), int(ant_state["y"]))
+				if verbose:
+					print(current_ant)
+
+		if verbose:
+			print(self)
 
 	def get_case_at(self, x, y):
 		return map_dict[(x, y)]
