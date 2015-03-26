46ccc62106ae62f7bfca9b6e7829315da4c15499
Olden Fabre <Fabre.Olden@gmail.com>
2015-03-26 17:11:31 +0100

    Changed a bit of the API to avoid compilation warnings
    (Added val at some places)

--- projet/MetaNoik/src/Messages.scala
+++ projet/MetaNoik/src/Messages.scala
@@ -1,28 +1,34 @@
 import akka.actor._
 
 abstract class Command
+/*
+ * These are the commands that can be sent to the ant
+ * each command has an identifier so that it can send a command
+ * to a precise ant
+ */
 
-case class Left(id: Int)
-case class Right(id: Int)
-case class Forward(id: Int)
-case class Rest(id: Int)
-case class Attack(id: Int, power: Int)
-case class Hack(id: Int, code: String)
+case class Left(id: Int) extends Command
+case class Right(id: Int) extends Command
+case class Forward(id: Int) extends Command
+case class Rest(id: Int) extends Command
+case class Attack(id: Int, power: Int) extends Command
+case class Hack(id: Int, code: String) extends Command
 
 abstract class Message
 
 /* server Interactions */
+
 /* IN */
 case class Auth(login: String, pass: String) extends Message
-case class Create(users: String,
-		  teaser: String,
-		  pace: Int,
-		  nb_turn: Int,
-		  nb_ant: Int,
-		  nb_player: Int,
-		  min: Int,
-		  energy: Int,
-		  acid: Int) extends Message
+case class Create(val users: String,
+                  val teaser: String,
+                  val pace: Int,
+                  val nb_turn: Int,
+                  val nb_ant: Int,
+                  val nb_player: Int,
+                  val min: Int,
+                  val energy: Int,
+                  val acid: Int) extends Message
 case class Destroy(id: String) extends Message
 case class Join(id: String) extends Message
 case class Logout() extends Message
@@ -33,9 +39,7 @@ case class Observation(turn: Int,
 		       obs: List[Pair[AntState, WorldMap]], 
 		       status: Status) extends Message
 
+case class Compute(obs: List[Pair[AntState,WorldMap]], status: Status) extends Message
 
-
-case class Compute(obs: List[Pair[AntState,Map]], status: Status)
-
-case class Ants(obs: List[Pair[AntState,Map]], status: Status)
-case class Map(obs: List[Pair[AntState,Map]], status: Status)
+case class Ants(obs: List[Pair[AntState,WorldMap]], status: Status) extends Message
+case class Map(obs: List[Pair[AntState,WorldMap]], status: Status) extends Message
--- projet/MetaNoik/src/Status.scala
+++ projet/MetaNoik/src/Status.scala
@@ -4,15 +4,21 @@ case class South() extends CardinalPoint
 case class East() extends CardinalPoint
 case class West() extends CardinalPoint
 
-class Pos(x: Int, y: Int)
+class Pos(val x: Int, val y: Int)
 
-class AntState(id: Int, controlled : Boolean, pos : Pos, orientation : CardinalPoint, hp : Int, mp : Int)
+class AntState(
+     val id : Int,
+     val controlled  : Boolean,
+     val pos         : Pos,
+     val orientation : CardinalPoint,
+     val hp          : Int,
+     val mp          : Int)
 
 abstract class MapSquare
 case class Grass() extends MapSquare
 case class Food(level: Int) extends MapSquare
 case class Water() extends MapSquare
 
-class WorldMap(squares: List[List[MapSquare]])
+class WorldMap(val squares: Array[Array[MapSquare]])
 
-class Status(turn: Int, map: WorldMap, ants: List[AntState])
+class Status(val turn: Int, val map: WorldMap, val ants: Array[AntState])
