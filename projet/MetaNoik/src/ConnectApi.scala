dc7a6b92313d2366b2b4b8785321b7920c345ea8
THIMON cynthia <thimon@informatique.univ-paris-diderot.fr>
2015-03-26 23:53:05 +0100

    update connection avec commandes fourmis

--- projet/MetaNoik/src/ConnectApi.scala
+++ projet/MetaNoik/src/ConnectApi.scala
@@ -1,14 +1,12 @@
 import scalaj.http._
-import spray.json._
-import DefaultJsonProtocol._ 
 
-class ConnectApi() {
+class ConnectApi {
+
   private val baseUrl = "http://yann.regis-gianas.org/antroid/"
   private val version = "0"
-
   //Création d'une requete basique
   def makeHttpRequest(requestName: String): HttpRequest = {
-    val request: HttpRequest = Http(baseUrl + version + "/" + requestName)
+    val request: HttpRequest = Http(baseUrl + version + "/" + requestName + "?")
     return request
   }
   // Envoie une requete avec la méthode POST
@@ -17,16 +15,28 @@ class ConnectApi() {
     return response.body
   }
 
+  //Envoie une requete avec la methode GET (sans parametres)
+  def getRequest(requestName: String): String = {
+    val response: HttpResponse[String] = makeHttpRequest(requestName).asString
+    return response.body
+  }
+
+  //Enregistrement d'un utilisateur
+  def registerUser(login: String, password: String): String = {
+    var params = Seq("login" -> login, "password" -> password)
+    return postRequest("register", params)
+  }
+
   // Authentification d'un utilisateur
   def auth(login: String, password: String): String = {
     var params = Seq("login" -> login, "password" -> password)
     return postRequest("auth", params)
   }
-	
-	//Enregistrement d'un utilisateur
-  def registerUser(login: String, password: String): String = {
-    var params = Seq("login" -> login, "password" -> password)
-    return postRequest("register", params)
+
+  //Deconnexion
+  def logout(): String = {
+    val response: HttpResponse[String] = makeHttpRequest("logout").asString
+    return response.body
   }
 
   //Creation d'un nouveau jeu
@@ -34,14 +44,8 @@ class ConnectApi() {
     minimal_nb_player: Int, initial_energy: Int, initial_acid: Int): String = {
     val response: HttpResponse[String] = makeHttpRequest("create?users="
       + users + "&teaser=" + teaser + "&pace=" + pace + "&nb_turn=" + nb_turn + "&nb_ant_per_player="
-      +nb_ant_per_player + "&nb_player=" + nb_player + "&minimal_nb_player=" + minimal_nb_player 
+      + nb_ant_per_player + "&nb_player=" + nb_player + "&minimal_nb_player=" + minimal_nb_player
       + "&initial_energy=" + initial_energy + "&initial_acid=" + initial_acid).asString
-    return response.body
-  }
-
-  // Destruction d'une partie
-  def destroyGame(idGame: String): String = {
-    val response: HttpResponse[String] = makeHttpRequest("destroy?id=" + idGame).asString
     return response.body
   }
 
@@ -51,29 +55,65 @@ class ConnectApi() {
     return response.body
   }
 
-  // status d'un game
-  def status(idGame: String): String = {
-    val response: HttpResponse[String] = makeHttpRequest("status?id=" + idGame).asString
+  // Liste des jeux visibles
+  def listGame(): String = {
+    return getRequest("games")
+  }
+
+  //Statut du jeu
+  def statusGame(idGame: String): String = {
+    return getRequest("status?id=" + idGame)
+  }
+
+  // Destruction d'une partie
+  def destroyGame(idGame: String): String = {
+    val response: HttpResponse[String] = makeHttpRequest("destroy?id=" + idGame).asString
     return response.body
   }
 
-  // Liste des jeux visibles
-  def listGame(): String = {
-    val response: HttpResponse[String] = makeHttpRequest("games").asString
+  //Statut de l'utilisateur dans le serveur
+  def whoami(): String = {
+    return getRequest("whoami")
+  }
+
+  /*Commandes pour les fourmis*/
+
+  def antCmd(idGame: String, action: String): String = {
+    val response: HttpResponse[String] = makeHttpRequest("play?id=" + idGame + "&cmds=left").asString
     return response.body
   }
+
+  def left(idGame: String): String = {
+    return antCmd(idGame, "left")
+  }
+
+  def right(idGame: String): String = {
+    return antCmd(idGame, "right")
+  }
+
+  def forward(idGame: String): String = {
+    return antCmd(idGame, "forward")
+  }
+
+  def rest(idGame: String): String = {
+    return antCmd(idGame, "rest")
+  }
+
+  def attack(idGame: String, level: Int): String = {
+    return antCmd(idGame, "attack@" + level)
+  }
+
 }
-// Test connect
+// Test connexion
 object test {
   def main(args: Array[String]) {
     val connection: ConnectApi = new ConnectApi()
     println("Hello, scala request!")
-    // println(connection.registerUser("test1", "test1"))
-    // println(connection.auth("test1", "test1"))
-    // println(connection.destroyGame("17262510196880258110079859391070411975"))
-    println(connection.newGame("all", "test", "10", 2, 3, 3, 2, 100, 100))
-    // println(connection.listGame())
-    // println(connection.joinAGame("4986356145915714886550832111036009180"))
-    // println(connection.status("4986356145915714886550832111036009180"))
+    //println(connection.registerUser("test1", "test1"))
+    println(connection.auth("test1", "test1"))
+    println(connection.newGame("all", "test", "10", 2, 3, 3, 2, 100, 1))
+    //println(connection.listGame())
+    println(connection.joinAGame("4986356145915714886550832111036009180"))
+    println(connection.left("4986356145915714886550832111036009180"))
   }
 }
